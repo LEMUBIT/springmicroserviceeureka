c4e67575e6d7554fc4aec06988b9b5839e4c7009
Ogbunude Lemuel <lemuelcco@gmail.com>
2019-10-01 00:28:42 +0100

    Initial commit

---
+++ src/test/java/lemubit/adacemy/springmicroserviceeureka/SpringmicroserviceeurekaApplicationTests.java
@@ -0,0 +1,16 @@
+package lemubit.adacemy.springmicroserviceeureka;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringmicroserviceeurekaApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
